79783b61a6e018d574f61bfa57151a8c283cbf34
ftonvi <onni.vitikainen@tuni.fi>
2023-03-06 04:24:35 +0200

    4.5

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,14 +1,35 @@
 const listHelper = require('../utils/list_helper')
-/*
+
+
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
-*/
+
+
 describe('total likes', () => {
   const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+})
+
+
+describe('favorite blog', () => {
+  const listWithBlogs = [
     {
       _id: '5a422a851b54a676234d17f7',
       title: 'React patterns',
@@ -59,8 +80,13 @@ describe('total likes', () => {
     }
   ]
 
-  test('when list has only one blog equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog)
-    expect(result).toBe(36)
+  test('when list has multiple blogs find favorite', () => {
+    const result = listHelper.favoriteBlog(listWithBlogs)
+    expect(result).toEqual(
+      {
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        likes: 12
+      })
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,35 @@
 /* eslint-disable */
-/*
+
+
 const dummy = (blogs) => {
 
-}*/
+}
 
 const totalLikes = (blogs) => {
     const result = blogs.map(blog => blog.likes).reduce((partialSum, a) => partialSum + a,0)
     return result
 }
 
+
+const favoriteBlog = (blogs) => {
+    const likeArray = blogs.map(blog => blog.likes)
+    console.log(likeArray)
+    const mostLiked = likeArray.reduce((a, b) => Math.max(a, b), -Infinity);
+    console.log(mostLiked)
+    const wantedBlog =  blogs.find(blog => blog.likes === mostLiked)
+    console.log(wantedBlog)
+    const wantedStruct = {
+        title: wantedBlog.title,
+        author: wantedBlog.author,
+        likes: wantedBlog.likes
+    }
+    console.log(wantedStruct)
+    return wantedStruct
+
+}
+
 module.exports = {
-  //dummy,
-  totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
