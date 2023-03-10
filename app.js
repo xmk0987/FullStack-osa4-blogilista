6d63bce5e06f5ebf66cfcc364847fcbc7c31f75e
ftonvi <onni.vitikainen@tuni.fi>
2023-03-10 14:08:48 +0200

    4.18

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('express-async-errors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 
 
@@ -32,8 +33,10 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use(middleware.requestLogger)
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
